a43c60d64a7fc70c0226a8d7ba574cc8f96e46ab
Sam Adkins <samuel.j.adkins@nga.mil>
2024-04-27 14:16:06 -0500

    basic structure complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Navigator from './Components/Navigator/Navigator.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <Navigator/>
+
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +30,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
---
+++ src/Components/Navigator/Navigator.jsx
@@ -0,0 +1,54 @@
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import adkinsLogo from "/src/Assets/Images/adkinsLogo.jpg";
+import "./Navigator.css";
+
+const Navigator = () => {
+  return (
+  <>
+    <Navbar expand={false} bg="dark" variant="dark" className="bg-body-tertiary mb-3">
+      <Container fluid style={{ background: "black" }}>
+        <Navbar.Brand href="#">
+          <Image
+          src={adkinsLogo}
+          roundedCircle
+          width="200"
+          height="200"
+          className="d-inline-block align-top"
+          alt="Adkins Music Logo"
+        />
+        </Navbar.Brand>
+        {/* TODO: FIGURE OUT USING CUSTOM FONTS */}
+        <Navbar.Text className="font-face-fr" style={{ fontSize: "280%", color: "#fff7e6" }}>Sam Adkins Music</Navbar.Text>
+        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand`} />
+        <Navbar.Offcanvas
+          id={`offcanvasNavbar-expand`}
+          aria-labelledby={`offcanvasNavbarLabel-expand`}
+          placement="end"
+          style={{ background: "black" }}
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title style={{ color: "white" }} id={`offcanvasNavbarLabel-expand`}>
+              Discover
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body style={{ background: "#b3cccc" }} >
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+              <Nav.Link href="#action1" >About</Nav.Link>
+              <Nav.Link href="#action2" >Music</Nav.Link>
+              <Nav.Link href="#action2" >Videos</Nav.Link>
+              <Nav.Link href="#action2" >Photos</Nav.Link>
+              <Nav.Link href="#action2" >Contact</Nav.Link>
+            </Nav>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+      </Container>
+    </Navbar>
+  </>
+  );
+};
+
+export default Navigator;
